23a871a05f66eb8cc64aa5414af73786867a355a
Kelvin Isievwore <kelvinator4leo@gmail.com>
2025-07-14 12:02:56 +0100

    ci fix

--- test.sh
+++ test.sh
@@ -7,6 +7,24 @@ set -e  # Exit immediately if any command fails
 
 # Spawn Bitcoind, and provide execution permission.
 docker compose up -d
+sleep 10
+
+echo "Waiting for bitcoind to be fully initialized..."
+
+while true; do
+  result=$(curl --silent --user alice:password --data-binary \
+    '{"jsonrpc":"1.0","id":"ping","method":"getblockchaininfo","params":[]}' \
+    -H 'content-type: text/plain;' http://127.0.0.1:18443)
+
+  if echo "$result" | grep -q '"chain"'; then
+    echo "bitcoind is ready."
+    break
+  else
+    echo "bitcoind not ready yet, retrying in 3s..."
+    sleep 3
+  fi
+done
+
 chmod +x ./rust/run-rust.sh
 chmod +x ./run.sh
 
